2ab530c188406861816eeb8af3ef2d89df3f37cc
pallaxa <2925792+Pallaxa@users.noreply.github.com>
2021-10-21 09:54:39 +0200

    added md based content

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const Metalsmith = require('metalsmith');
+const markdown = require('metalsmith-markdown')
+const layouts = require('metalsmith-layouts')
 
 Metalsmith(__dirname)
     .source('./src')
+    .use(markdown())
+    .use(
+        layouts({
+            default: 'master.hbs',
+            engine: 'handlebars'
+        })
+    )
     .destination('./docs')  
     .clean(true)             
     .build(function (err) {  
