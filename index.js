87e94ac685fbc22c5f6ea1e21e198a3838d9f03e
pallaxa <2925792+Pallaxa@users.noreply.github.com>
2021-10-21 10:26:46 +0200

    added permalinks for later usage

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const Metalsmith = require('metalsmith');
 const markdown = require('metalsmith-markdown')
 const layouts = require('metalsmith-layouts')
 const partials = require('metalsmith-discover-partials')
+const permalinks = require('metalsmith-permalinks')
 
 Metalsmith(__dirname)
     .source('./src')
     .use(partials())
+    .use(
+        permalinks({
+            pattern: ':title'
+        })
+    )
     .use(markdown())
     .use(
         layouts({
